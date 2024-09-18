66904cc613901225b9de7f9af1df7e0092b6642b
kietn20 <kietnguyen3698@gmail.com>
2024-09-18 13:24:42 -0700

    worked on StockBroker.cs

--- Lab 1/StockNotification.cs
+++ Lab 1/StockNotification.cs
@@ -9,14 +9,14 @@ namespace Stock
         public string StockName { get; set; }
         public int CurrentValue { get; set; }
         public int NumChanges { get; set; }
+
         /// <summary>
         /// Stock notification attributes that are set and changed
         /// </summary>
         /// <param name="stockName">Name of stock</param>
         /// <param name="currentValue">Current vallue of the stock</param>
         /// <param name="numChanges">Number of changes the stock goes through</param>
-        public StockNotification(string stockName, int currentValue, int
-        numChanges)
+        public StockNotification(string stockName, int currentValue, int numChanges)
         {
             // !NOTE!: Fill in below of what the notification will do using the comments above
             this.StockName = stockName;
